509f9928bd8cba48dacb0e27951bf2e94a26feef
beafb <beafbeaccon@yahoo.com>
2018-03-16 11:57:36 +0100

    final

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#coding:utf8
 from flask import Flask, render_template, request
 import requests
 
@@ -6,9 +7,10 @@ webhook_url = 'https://hooks.slack.com/services/T0H6BQ6UU/B9PS1RKH6/y292xUDh5Tre
 
 
 def slackthat(mail=None, name=None, poste=None, job=None, company=None):
+    print 'okok'
     print 'Mail -> %s' % mail
     if job == 1:
-        data = {"text":"@Nico Il est chaud :fire: :fire: :fire: : \nNom = %s\nPoste = %s\nChez = %s\nMail = %s" % (name, poste, company, mail)}
+        data = {"text":"Il est chaud :fire: :fire: :fire: : \nNom = %s\nPoste = %s\nChez = %s\nMail = %s" % (name, poste, company, mail)}
     else:
         data = {"text":"Il est chaud :fire: :fire: :fire: : \nNom = %s\nPoste = %s\nPhone = %s" % (name, poste, mail)}
     r = requests.post(webhook_url, json=data)
@@ -23,19 +25,20 @@ def slackthat(mail=None, name=None, poste=None, job=None, company=None):
 def hello():
     if request.method == 'POST':
         print request.form['name']
-        if request.form['mail']:
+        print request.form
+        if request.form['media'] == "1":
+            print 'if'
             contact = request.form['mail']
             job = 1
         else:
+            print 'else'
             contact = request.form['phone']
             job = 2
-        print request.form
-        ret = slackthat(contact, request.form['name'], request.form['job'], job, request.form['company'])
-        print ret
+        slackthat(contact, request.form['name'], request.form['job'], job, 'Klee Group')
         return render_template('thanks.html', company=request.form['company'])
     else:
         return render_template('job.html', company='Klee Group', linkedin='17552')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
